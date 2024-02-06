57b8aae5dbcab92cf70e4db0c3ff7cb233808c5f
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-06 11:59:00 +0100

    prueba java_v2

--- untitled/src/entidades/Producto.java
+++ untitled/src/entidades/Producto.java
@@ -2,8 +2,49 @@ package entidades;
 
 public class Producto {
     int idProduct;
-    String nameProduct;
-    String descriptionProduct;
-    double priceProduct;
-    int stockProduct;
+    String titulo;
+    String desc;
+    float precio;
+
+    public Producto() {
+    }
+
+    public Producto(String titulo, String desc, float precio) {
+        this.titulo = titulo;
+        this.desc = desc;
+        this.precio = precio;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
 }
---
+++ untitled/src/repositories/ProductRepositoryDB.java
@@ -0,0 +1,32 @@
+package repositories;
+
+import entidades.Producto;
+import interfaces.Repository;
+
+public class ProductRepositoryDB implements Repository<Producto> {
+
+    @Override
+    public void save(Producto entity) {
+
+    }
+
+    @Override
+    public void find() {
+
+    }
+
+    @Override
+    public void findOne() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
